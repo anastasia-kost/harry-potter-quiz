3a668f4bf18dafeaf7067d2b6d6d99934b04adbc
anastasia-kost <voznikova_a@mail.ru>
2021-08-23 18:01:38 +0300

    implemented JS functionality

--- main.js
+++ main.js
@@ -80,3 +80,80 @@ const quizData = [
     correct: "c",
   },
 ];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      if (score === quizData.length) {
+        quiz.innerHTML = `
+      <h2>Congratulations! You know everything about The Boy Who Lived!<br>${score}/${quizData.length} </h2>
+      <button onClick="location.reload()">Reload</button>
+      `;
+      }
+      if (score < quizData.length) {
+        quiz.innerHTML = `
+      <h2>Good job! But you should restore your knowledge about Harry Potter and his friends!<br>${score}/${quizData.length} </h2>
+      <button onClick="location.reload()">Reload</button>
+      `;
+      }
+      if (score < 5) {
+        quiz.innerHTML = `
+      <h2>Oh no! It's time to review movies or reread books! Good luck!<br>${score}/${quizData.length} </h2>
+      <button onClick="location.reload()">Reload</button>
+      `;
+      }
+    }
+  }
+});
+
+//You scored ${score} correct answers out of ${quizData.length}</h2>
